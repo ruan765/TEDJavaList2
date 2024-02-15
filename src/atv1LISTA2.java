bb3dd74bf9c7a05aafa0b65b546f07ed00156367
Ruan pablo <ruansaraiva555@gmail.com>
2024-02-15 15:15:59 -0300

    Iniciarlização

---
+++ src/atv1LISTA2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class atv1LISTA2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i<20; i++) {
+            System.out.println(i);
+        }
+        for (int i = 1; i<=20; i++) {
+            System.out.print(" " + i );
+        }
+
+
+        sc.close();
+    }
+}
---
+++ src/atv2LISTA2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class atv2LISTA2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maior = 0;
+
+        int[] numbers = new int[5];
+
+        for(int i = 0; i<numbers.length; i++) {
+            System.out.printf("numero %d:" , i+1);
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i<numbers.length; i++) {
+            if (maior < numbers[i]) {
+                maior = numbers[i];
+                
+            }
+        }
+        System.out.println("O maior numero é " + maior);
+
+
+        sc.close();
+    }
+    
+}
