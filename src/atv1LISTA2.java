859d38347e7337c7ea952ff65992f3db57e57387
ruan pablo <105497678+ruan765@users.noreply.github.com>
2024-03-04 17:01:45 -0300

    Update atv1LISTA2.java

--- src/atv1LISTA2.java
+++ src/atv1LISTA2.java
@@ -7,7 +7,7 @@ public class atv1LISTA2 {
             System.out.println(i);
         }
         for (int i = 1; i<=20; i++) {
-            System.out.print(" " + i );
+            System.out.println(" " + i );
         }
 
 
